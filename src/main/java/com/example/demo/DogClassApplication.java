fecb7f5b8df09d946892bc70369f68be5f487fb6
Nasq23 <honey.menu23@gmail.com>
2019-08-28 12:00:06 -0400

    dog names

---
+++ src/main/java/com/example/demo/DogClassApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogClassApplication.class, args);
+	}
+
+}
